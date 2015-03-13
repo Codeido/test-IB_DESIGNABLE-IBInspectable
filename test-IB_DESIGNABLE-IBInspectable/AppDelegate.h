7c6d1bd0f16c50553013899579d76f07480252a1
PAOLO MUSOLINO <info@codeido.com>
2015-03-13 14:21:48 +0100

    Initial Commit

---
+++ test-IB_DESIGNABLE-IBInspectable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-IB_DESIGNABLE-IBInspectable
+//
+//  Created by Paolo Musolino on 13/03/15.
+//  Copyright (c) 2015 Codeido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test-IB_DESIGNABLE-IBInspectable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test-IB_DESIGNABLE-IBInspectable
+//
+//  Created by Paolo Musolino on 13/03/15.
+//  Copyright (c) 2015 Codeido. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
